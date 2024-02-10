32d140fc731fd85679f07da2e6c4d098ad8da700
lumenwrites <lumenwrites@gmail.com>
2024-02-10 15:35:54 +0400

    Ignore ts errors

--- src/suggesters/FileSuggester.ts
+++ src/suggesters/FileSuggester.ts
@@ -2,74 +2,69 @@
 
 import { TAbstractFile, TFile } from "obsidian";
 import { TextInputSuggest } from "./suggest";
-import { get_tfiles_from_folder } from "utils/Utils";
+import { get_tfiles_from_folder } from "src/utils/Utils";
 import TemplaterPlugin from "main";
-import { errorWrapperSync } from "utils/Error";
+import { errorWrapperSync } from "src/utils/Error";
 
 export enum FileSuggestMode {
-    TemplateFiles,
-    ScriptFiles,
+	TemplateFiles,
+	ScriptFiles,
 }
 
 export class FileSuggest extends TextInputSuggest<TFile> {
-    constructor(
-        public inputEl: HTMLInputElement,
-        private plugin: TemplaterPlugin,
-        private mode: FileSuggestMode
-    ) {
-        super(inputEl);
-    }
+	constructor(
+		public inputEl: HTMLInputElement,
+		private plugin: TemplaterPlugin,
+		private mode: FileSuggestMode
+	) {
+		super(inputEl);
+	}
 
-    get_folder(mode: FileSuggestMode): string {
-        switch (mode) {
-            case FileSuggestMode.TemplateFiles:
-                return this.plugin.settings.templates_folder;
-            case FileSuggestMode.ScriptFiles:
-                return this.plugin.settings.user_scripts_folder;
-        }
-    }
+	get_folder(mode: FileSuggestMode): string {
+		return this.plugin.settings.folderPath;
+	}
 
-    get_error_msg(mode: FileSuggestMode): string {
-        switch (mode) {
-            case FileSuggestMode.TemplateFiles:
-                return `Templates folder doesn't exist`;
-            case FileSuggestMode.ScriptFiles:
-                return `User Scripts folder doesn't exist`;
-        }
-    }
+	get_error_msg(mode: FileSuggestMode): string {
+		switch (mode) {
+			case FileSuggestMode.TemplateFiles:
+				return `Templates folder doesn't exist`;
+			case FileSuggestMode.ScriptFiles:
+				return `User Scripts folder doesn't exist`;
+		}
+	}
 
-    getSuggestions(input_str: string): TFile[] {
-        const all_files = errorWrapperSync(
-            () => get_tfiles_from_folder(this.get_folder(this.mode)),
-            this.get_error_msg(this.mode)
-        );
-        if (!all_files) {
-            return [];
-        }
+	getSuggestions(input_str: string): TFile[] {
+		const all_files = errorWrapperSync(
+			() => get_tfiles_from_folder(this.get_folder(this.mode)),
+			this.get_error_msg(this.mode)
+		);
+		if (!all_files) {
+			return [];
+		}
 
-        const files: TFile[] = [];
-        const lower_input_str = input_str.toLowerCase();
+		const files: TFile[] = [];
+		const lower_input_str = input_str.toLowerCase();
 
-        all_files.forEach((file: TAbstractFile) => {
-            if (
-                file instanceof TFile &&
-                file.extension === "md" &&
-                file.path.toLowerCase().contains(lower_input_str)
-            ) {
-                files.push(file);
-            }
-        });
+		all_files.forEach((file: TAbstractFile) => {
+			if (
+				file instanceof TFile &&
+				file.extension === "md" &&
+				file.path.toLowerCase().contains(lower_input_str)
+			) {
+				files.push(file);
+			}
+		});
 
-        return files;
-    }
+		return files;
+	}
 
-    renderSuggestion(file: TFile, el: HTMLElement): void {
-        el.setText(file.path);
-    }
+	renderSuggestion(file: TFile, el: HTMLElement): void {
+		el.setText(file.path);
+	}
 
-    selectSuggestion(file: TFile): void {
-        this.inputEl.value = file.path;
-        this.inputEl.trigger("input");
-        this.close();
-    }
+	selectSuggestion(file: TFile): void {
+		this.inputEl.value = file.path;
+		this.inputEl.trigger("input");
+		this.close();
+	}
 }
